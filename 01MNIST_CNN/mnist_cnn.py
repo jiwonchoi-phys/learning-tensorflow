ab81b5ad9b7a211b4779f57fd3efe7c6c47a7d21
jiwon Choi <67862418+jiwonchoi-phys@users.noreply.github.com>
2020-07-06 00:26:55 +0900

    Add files via upload

---
+++ 01MNIST_CNN/mnist_cnn.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  1 22:41:23 2020
+
+@author: jiwon
+"""
+
+
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+
+# Load training data(MNIST)
+(train_images, train_labels), (test_images, test_labels) = \
+    datasets.mnist.load_data()
+
+# Reshape input data
+train_images = train_images.reshape((60000,28,28,1))
+test_images = test_images.reshape((10000,28,28,1))
+
+# Normalize RGB 0~255 to 0~1
+train_images, test_images = train_images/255., test_images/255.
+
+
+# Build the model
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape = (28,28,1)))
+model.add(layers.MaxPooling2D((2,2)))
+
+model.add(layers.Conv2D(64, (3,3), activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+
+model.add(layers.Conv2D(64, (3,3), activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
+# Get Description about model
+model.summary()
+
+# Compile
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+
+checkpoint_path = "MNIST_CNN/cp-{epoch:04d}.ckpt"
+
+
+cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
+
+model.fit(train_images, train_labels, epochs=5,
+          callbacks = [cp_callback])
+
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+print(test_acc)
+model.save('MNIST_CNN.h5')
